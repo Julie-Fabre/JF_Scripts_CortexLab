861778182311191324ec7eaa6f87d6048a5a0c8c
Julie-Fabre <julie.m.fabre@gmail.com>
2020-11-04 13:40:46 +0000

    tidyingCode

---
+++ behavior/forYeqing_choiceworld_behavior_JF.m
@@ -0,0 +1,336 @@
+%% Get and plot single mouse behavior (vanillaChoiceworld and choiceWorldJF)
+corona = 0;
+animalsAll = { 'JF014', 'JF015'}; %chnage to the names of the animals you want here 
+for iAnimal = 1:size(animalsAll,2)
+
+    animal = animalsAll{1,iAnimal}; %this animal
+    protocol = 'choiceworld';%protocol name contains this name 
+    flexible_name = true;%protocol name can be slightly different 
+    experiments = AP_find_experimentsJF(animal,protocol, protocol);%find the experiments with the choiceworld protocol
+   
+    bhv = struct;%initialize structure 
+
+    for curr_day = 1:length(experiments)
+        
+        day = experiments(curr_day).day;
+        experiment_num = experiments(curr_day).experiment;
+
+        % If multiple experiments, only use the last one (usually multiple
+        % happens if mess ups and final one is good)
+        for curr_experiment = length(experiment_num)
+
+            experiment = experiment_num(curr_experiment);
+
+            [block_filename, block_exists] = AP_cortexlab_filenameJF(animal,day,experiment,'block');
+
+            % Load the block file
+            load(block_filename)
+            
+            % Get protocol name
+            [~,protocol] = fileparts(block.expDef);
+
+            % Time of session (in minutes)
+            session_duration = block.duration/60;
+
+            % Trial counts
+            n_trials = length(block.paramsValues);
+            total_water = sum(block.outputs.rewardValues);
+
+            % Estimate reaction time
+            % (evenly resample velocity - not even natively);
+            wheel_resample_rate = 1000;
+            wheel_t_resample = block.inputs.wheelTimes(1):1/wheel_resample_rate:block.inputs.wheelTimes(end);
+            wheel_values_resample = interp1(block.inputs.wheelTimes,block.inputs.wheelValues,wheel_t_resample);
+
+            wheel_smooth_t = 0.05; % seconds
+            wheel_smooth_samples = round(wheel_smooth_t*wheel_resample_rate);
+            wheel_velocity = interp1(conv(wheel_t_resample,[1,1]/2,'valid'), ...
+                diff(smooth(wheel_values_resample,wheel_smooth_samples)),wheel_t_resample)';
+
+            wheel_thresh = 0.025;        
+            wheel_starts = wheel_t_resample(abs(wheel_velocity(1:end-1)) < wheel_thresh & ...
+                abs(wheel_velocity(2:end)) > wheel_thresh);       
+
+            response_trials = 1:length(block.events.responseValues);
+            if isfield(block.events, 'stimOnTimes')
+                trial_wheel_starts = arrayfun(@(x) ...
+                    wheel_starts(find(wheel_starts > block.events.stimOnTimes(x),1)), ...
+                    response_trials);      
+                trial_move_t = trial_wheel_starts - block.events.stimOnTimes(response_trials);
+               %  removeThis = find(cellfun(@isempty,trial_wheel_starts));
+            else
+              % block.events.stimulusOnTimes = block.events.stimulusOnTimes(1:length(response_trials));
+               trial_wheel_starts = arrayfun(@(x) ...
+                   wheel_starts(find(wheel_starts > block.events.stimulusOnTimes(x),1)), ...
+                   response_trials, 'UniformOutput',false);  
+               removeThis = find(cellfun(@isempty,trial_wheel_starts));
+               trial_wheel_starts = trial_wheel_starts(find(~cellfun(@isempty,trial_wheel_starts))); %remove empty, and back to vector 
+               trial_wheel_starts = cell2mat(trial_wheel_starts);
+               trial_move_t = trial_wheel_starts - block.events.stimulusOnTimes(response_trials(1:end-length(removeThis)));
+            end
+            % Wheel movements/biases
+            left_wheel_velocity = abs(wheel_velocity.*(wheel_velocity < 0));
+            right_wheel_velocity = abs(wheel_velocity.*(wheel_velocity > 0));
+            wheel_bias = (nansum(right_wheel_velocity)-nansum(left_wheel_velocity))/ ...
+                (nansum(right_wheel_velocity)+nansum(left_wheel_velocity));
+
+            % Get reaction times for each stimulus
+            if isfield(block.events, 'trialContrastValues')
+                trial_stim = block.events.trialContrastValues(response_trials).*block.events.trialSideValues(response_trials);
+                stim_list = unique(reshape(unique(block.events.contrastsValues).*[-1;1],[],1));
+                [~,trial_stim_idx] = ismember(trial_stim,stim_list);
+                stim_rxn_time = accumarray(trial_stim_idx(response_trials(1:end))',trial_move_t',[11,1],@nanmedian,nan);
+             
+            else
+                
+                trial_stim = block.events.contrastLeftValues(response_trials) -block.events.contrastRightValues(response_trials);
+                stim_list = unique(reshape(unique([block.events.contrastSetValues, -block.events.contrastSetValues]).*[-1;1],[],1));
+                [~,trial_stim_idx] = ismember(trial_stim,stim_list);
+                stim_rxn_time = accumarray(trial_stim_idx(response_trials(1:end-length(removeThis)))',trial_move_t',[11,1],@nanmedian,nan);
+             
+            end
+            
+           
+               
+             
+            % Performance (note that this excludes repeat on incorrect trials)
+            if isfield(block.events, 'sessionPerformanceValues')
+                performance = block.events.sessionPerformanceValues(:,end-10:end);
+                
+                %save performance
+                bhv.conditions = performance(1,:);
+                bhv.n_trials_condition(curr_day,:) = performance(2,:);
+                bhv.go_left_trials(curr_day,:) = performance(end,:);
+
+                   if ~isempty(block.events.totalWaterValues)
+                  bhv.rewardValue(curr_day)=block.events.totalWaterValues(1);
+                   else
+                       bhv.rewardValue(curr_day)=NaN;
+                   end
+                  bhv.stim_rxn_time(curr_day,:) = stim_rxn_time;
+            else
+              % performance = block.events.sessionPerformanceValues(:,end-10:end);
+                
+                %save performance
+                bhv.conditions = unique(reshape(unique([block.events.contrastSetValues, -block.events.contrastSetValues]).*[-1;1],[],1));
+                if numel(block.events.repeatNumValues) == numel(block.events.feedbackValues)
+                    %repeatOnMisses = zeros(size(block.events.feedbackValues,2),1);
+                    %repeatOnMisses = [block.events.repeatNumValues(2:end) > 1 & block.events.feedbackValues(1:end-1)==-1, 0];%zeros(size(block.events.feedbackValues,1),1);%
+                else
+                    %repeatOnMisses = zeros(size(block.events.feedbackValues));all
+                    %repeatOnMisses = [block.events.repeatNumValues(2:end-1) > 1 & block.events.feedbackValues(1:end-1)==-1, 0];%zeros(size(block.events.feedbackValues,1),1);%
+                end
+                
+                for iCond = 1:size(bhv.conditions,1)
+                    bhv.n_trials_condition(curr_day,iCond) = numel(find(trial_stim==bhv.conditions(iCond))) ;
+                    bhv.n_trials_condition_not_repeat(curr_day,iCond) = numel(find(trial_stim==bhv.conditions(iCond)));%&~repeatOnMisses
+                    bhv.go_left_trials(curr_day,iCond) = numel(find(trial_stim==bhv.conditions(iCond) & block.events.responseValues==-1 ));%&~repeatOnMisses
+                end
+                performance = bhv.go_left_trials./bhv.n_trials_condition_not_repeat;%removing repeat on miss trials
+                
+                if contains(block.expDef, 'rew') %version of the task where manual rewards can be inputed 
+                   manualRew = length(strfind(block.inputs.keyboardValues,'r')); 
+                else
+                   manualRew = 0; 
+                end
+                rSize = block.paramsValues.rewardSize;
+                bhv.rewardValue(curr_day)=sum(block.outputs.rewardValues) - (manualRew*rSize);
+                   
+                bhv.stim_rxn_time(curr_day,:) = stim_rxn_time;
+                    
+               
+                
+               
+                
+             
+            end
+            
+            % Get whether all contrasts were used
+            use_all_contrasts = all(block.events.useContrastsValues(end-10:end));
+
+            % Store in behavior structure
+            bhv.protocol{curr_day} = protocol;
+            bhv.session_duration(curr_day) = session_duration;
+            bhv.n_trials(curr_day) = n_trials;
+            bhv.total_water(curr_day) = total_water;
+            bhv.wheel_velocity(curr_day) = nansum(abs(wheel_velocity));
+            bhv.wheel_bias(curr_day) = wheel_bias;
+            bhv.use_all_contrasts(curr_day) = use_all_contrasts;
+            
+
+            AP_print_progress_fractionJF(curr_day,length(experiments));
+        end
+
+    end
+    
+    % Plot summary
+    day_num = cellfun(@(x) datenum(x),{experiments.day});
+    day_labels = cellfun(@(day,protocol) [protocol(19:end) ' ' day(6:end)], ...
+        {experiments.day},bhv.protocol,'uni',false);
+    figure('Name',animal)
+
+    % Trials and water
+    combine_days = bhv.use_all_contrasts;
+    %combine_days=0;
+
+
+        subplot(321)
+
+    
+    yyaxis left
+
+    plot(day_num,bhv.n_trials,'linewidth',2);
+    ylabel('Trials');
+    yyaxis right
+    
+    plot(day_num,bhv.total_water,'linewidth',2);
+    ax=gca;
+    hold on;
+
+    ylabel('Total water (ul)');
+    xlabel('Session');
+    set(gca,'XTick',day_num);
+    set(gca,'XTickLabel',day_labels);
+    set(gca,'XTickLabelRotation',90);
+
+    imaging_days = day_num([experiments.imaging]);
+    for i = 1:length(imaging_days)
+        line(repmat(imaging_days(i),1,2),ylim,'color','k');
+    end
+
+    ephys_days = day_num([experiments.ephys]);
+    for i = 1:length(ephys_days)
+        line(repmat(ephys_days(i),1,2),ylim,'color','r','linestyle','--');
+    end
+
+    % Wheel movement and bias
+
+        subplot(322)
+
+    
+    yyaxis left
+
+    plot(day_num,bhv.wheel_velocity,'linewidth',2);
+    ylabel('Wheel movement');
+    yyaxis right
+    plot(day_num,bhv.wheel_bias,'linewidth',2);
+    ylim([-1,1]);
+    line(xlim,[0,0]);
+    ylabel('Wheel bias');
+    xlabel('Session');
+    set(gca,'XTick',day_num);
+    set(gca,'XTickLabel',day_labels);
+    set(gca,'XTickLabelRotation',90);
+
+    imaging_days = day_num([experiments.imaging]);
+    for i = 1:length(imaging_days)
+        line(repmat(imaging_days(i),1,2),ylim,'color','k');
+    end
+
+    ephys_days = day_num([experiments.ephys]);
+    for i = 1:length(ephys_days)
+        line(repmat(ephys_days(i),1,2),ylim,'color','r','linestyle','--');
+    end
+
+    % Psychometric over all days
+
+        subplot(323)
+
+
+    im = imagesc(bhv.conditions,1:size(bhv.go_left_trials),bhv.go_left_trials./bhv.n_trials_condition, [0 1]);
+    set(im,'AlphaData',~isnan(get(im,'CData')));
+    set(gca,'color',[0.5,0.5,0.5]);
+    colormap(brewermap([],'*RdBu'));
+    c = colorbar;
+    caxis= [0 1];
+    ylabel(c,'Go left (frac)');
+    xlabel('Condition');
+    ylabel('Session');
+    set(gca,'YTick',1:length(experiments));
+    set(gca,'YTickLabel',day_labels);
+    axis square;
+    hold on;
+    if any([experiments.imaging])
+        plot(0,find([experiments.imaging]),'.k');
+    end
+    if any([experiments.ephys])
+        plot(0,find([experiments.ephys]),'ok');
+    end
+
+    % Reaction time over days
+
+        subplot(324)
+
+
+    im = imagesc(bhv.conditions,1:size(bhv.go_left_trials),bhv.stim_rxn_time);
+    set(im,'AlphaData',~isnan(get(im,'CData')));
+    set(gca,'color',[0.5,0.5,0.5]);
+    colormap(brewermap([],'*RdBu'));
+   % caxis([0.2,0.8])
+    c = colorbar;
+    ylabel(c,'Reaction time');
+    xlabel('Condition');
+    ylabel('Session');
+    set(gca,'YTick',1:length(experiments));
+    set(gca,'YTickLabel',day_labels);
+    axis square;
+    hold on;
+    if any([experiments.imaging])
+        plot(0,find([experiments.imaging]),'.k');
+    end
+    if any([experiments.ephys])
+        plot(0,find([experiments.ephys]),'ok');
+    end
+subplot(3,2,5); hold on;
+    
+        last_days_performance = bhv.go_left_trials(end,:)./bhv.n_trials_condition(end,:);
+        errorbar(bhv.conditions,nanmean(last_days_performance,1), ...
+            nanstd(last_days_performance,[],1)./sqrt(sum(~isnan(last_days_performance))),'k','linewidth',2);
+        xlim([-1,1]);
+        ylim([0,1]);
+        line(xlim,[0.5,0.5],'color','k','linestyle','--');
+        line([0,0],ylim,'color','k','linestyle','--');
+        axis square;
+        xlabel('Condition');
+        ylabel('Fraction go left');
+
+        % Reaction time of combined days that use all contrasts
+        subplot(3,2,6); hold on;
+       % combine_days = bhv.use_all_contrasts;
+        last_days_rxn_time = bhv.stim_rxn_time(end,:);
+        errorbar(bhv.conditions,nanmean(last_days_rxn_time,1), ...
+            nanstd(last_days_rxn_time,[],1)./sqrt(sum(~isnan(last_days_rxn_time))),'k','linewidth',2);
+        xlim([-1,1]);
+        line(xlim,[0.5,0.5],'color','k','linestyle','--');
+        axis square;
+        xlabel('Condition');
+        ylabel('Reaction time');
+    % Psychometric of combined days that use all contrasts
+    if sum(combine_days)~=0
+        subplot(3,2,5); hold on;
+    
+        combine_days_performance = bhv.go_left_trials(combine_days,:)./bhv.n_trials_condition(combine_days,:);
+        errorbar(bhv.conditions,nanmean(combine_days_performance,1), ...
+            nanstd(combine_days_performance,[],1)./sqrt(sum(~isnan(combine_days_performance))),'k','linewidth',2);
+        xlim([-1,1]);
+        ylim([0,1]);
+        line(xlim,[0.5,0.5],'color','k','linestyle','--');
+        line([0,0],ylim,'color','k','linestyle','--');
+        axis square;
+        xlabel('Condition');
+        ylabel('Fraction go left');
+
+        % Reaction time of combined days that use all contrasts
+        subplot(3,2,6); hold on;
+        combine_days = bhv.use_all_contrasts;
+        combine_days_rxn_time = bhv.stim_rxn_time(combine_days,:);
+        errorbar(bhv.conditions,nanmean(combine_days_rxn_time,1), ...
+            nanstd(combine_days_rxn_time,[],1)./sqrt(sum(~isnan(combine_days_rxn_time))),'k','linewidth',2);
+        xlim([-1,1]);
+        line(xlim,[0.5,0.5],'color','k','linestyle','--');
+        axis square;
+        xlabel('Condition');
+        ylabel('Reaction time');
+    end
+    
+end
---
+++ load/AP_align_striatum_ephysJF.m
@@ -0,0 +1,201 @@
+function [str_depth,aligned_str_depth_group] = AP_align_striatum_ephysJF
+% Align striatum ephys across experiments
+%
+% requires:
+% str_align - 'depth' or 'kernel'
+% 'depth' requires n_aligned_depths
+
+%% Pull variables from base workspace
+% (bad practice - but this used to be a script instead of a function and I
+% wanted to not store all the intermediate variables because they're
+% common)
+
+animal = evalin('base','animal');
+day = evalin('base','day');
+spike_times = evalin('base','spike_times');
+spike_templates = evalin('base','spike_templates');
+spike_depths = evalin('base','spike_depths');
+template_depths = evalin('base','template_depths');
+verbose = evalin('base','verbose');
+str_align = evalin('base','str_align');
+n_aligned_depths = evalin('base','n_aligned_depths');
+
+%% Get striatum boundaries
+
+%%% Get correlation of MUA in sliding sindows
+depth_corr_window = 100; % MUA window in microns
+depth_corr_window_spacing = 50; % MUA window spacing in microns
+
+max_depths = 3840; % (hardcode, sometimes kilosort2 drops channels)
+
+depth_corr_bins = [0:depth_corr_window_spacing:(max_depths-depth_corr_window); ...
+    (0:depth_corr_window_spacing:(max_depths-depth_corr_window))+depth_corr_window];
+depth_corr_bin_centers = depth_corr_bins(1,:) + diff(depth_corr_bins,[],1)/2;
+
+spike_binning_t = 0.01; % seconds
+spike_binning_t_edges = nanmin(spike_times):spike_binning_t:nanmax(spike_times);
+
+binned_spikes_depth = zeros(size(depth_corr_bins,2),length(spike_binning_t_edges)-1);
+for curr_depth = 1:size(depth_corr_bins,2)
+    curr_depth_templates_idx = ...
+        find(template_depths >= depth_corr_bins(1,curr_depth) & ...
+        template_depths < depth_corr_bins(2,curr_depth));
+    
+    binned_spikes_depth(curr_depth,:) = histcounts(spike_times( ...
+        ismember(spike_templates,curr_depth_templates_idx)),spike_binning_t_edges);
+end
+
+mua_corr = corrcoef(binned_spikes_depth');
+
+
+%%% Estimate start and end depths of striatum
+
+% % end of striatum: biggest (smoothed) drop in MUA correlation near end
+% groups_back = 30;
+% mua_corr_end = medfilt2(mua_corr(end-groups_back+1:end,end-groups_back+1:end),[3,3]);
+% mua_corr_end(triu(true(length(mua_corr_end)),0)) = nan;
+% median_corr = medfilt1(nanmedian(mua_corr_end,2),3);
+% [x,max_corr_drop] = min(diff(median_corr));
+% str_end = depth_corr_bin_centers(end-groups_back+max_corr_drop);
+
+% (new method)
+% end of striatum: minimum correlation on dim 1 * dim 2
+% (to look for the biggest dead space between correlated blocks)
+groups_back = 20;
+mua_corr_end = medfilt2(mua_corr(end-groups_back+1:end,end-groups_back+1:end),[3,3]);
+mua_corr_end(triu(true(length(mua_corr_end)),0)) = nan;
+mean_corr_dim1 = nanmean(mua_corr_end,2);
+mean_corr_dim2 = nanmean(mua_corr_end,1);
+mean_corr_mult = mean_corr_dim1.*mean_corr_dim2';
+[~,mean_corr_mult_min_idx] = min(mean_corr_mult);
+str_end = depth_corr_bin_centers(end-groups_back + mean_corr_mult_min_idx - 2); % err early: back up 2 (100 um)
+
+% start of striatum: look for ventricle
+% (by biggest gap between templates)
+min_gap = 200;
+sorted_template_depths = sort([0;template_depths]);
+[max_gap,max_gap_idx] = max(diff(sorted_template_depths));
+if max_gap > min_gap
+    str_start = sorted_template_depths(max_gap_idx+1)-1;
+else
+    str_start = sorted_template_depths(2);
+end
+
+str_depth = [str_start,str_end];
+
+%% Group striatal units by alignment
+
+switch str_align
+    
+    case 'none'
+        %%% Don't do anything
+        
+    case 'depth'
+        %%% Align striatal recordings using saved alignment
+        % (this requires an input for n_aligned_depths)
+        % (NEW WAY: hardcode striatum length, doesn't need anything saved)
+        if exist('n_aligned_depths','var')
+            ephys_align_path = 'C:\Users\Andrew\OneDrive for Business\Documents\CarandiniHarrisLab\analysis\wf_ephys_choiceworld\ephys_processing';
+            ephys_align_fn = ['ephys_depth_align.mat'];
+            if exist([ephys_align_path filesep ephys_align_fn],'file')
+                load([ephys_align_path filesep ephys_align_fn]);
+                % Only use if this animal was included in the alignment
+                curr_animal_idx = strcmp(animal,{ephys_depth_align.animal});
+                if any(curr_animal_idx)
+                    curr_day_idx = strcmp(day,ephys_depth_align(curr_animal_idx).day);
+                    if any(curr_day_idx)
+                        if verbose; disp('Aligning striatum by saved depths...'); end
+                        
+%                         % Get max striatum length and divide evenly
+%                         all_str_lengths = diff(vertcat(ephys_depth_align(:).str_depth),[],2);
+%                         max_str_length = max(all_str_lengths);
+%                         global_str_depth_edges = linspace(0,max_str_length,n_aligned_depths+1);
+                        
+                        % ALTERNATE: set max striatum length, divide evenly
+                        max_str_length = 3000; % Based on CCF trajectory
+                        global_str_depth_edges = linspace(0,max_str_length,n_aligned_depths+1);
+                        
+                        % Set current striatum depth edges by global length
+                        % assuming that the end is actually the end
+                        str_depth_edges = sort(str_depth(2) - global_str_depth_edges);
+                        str_depth_edges(1) = -Inf; % In case longer than max 
+                        
+                        % Get spike depths, setting all outside the striatum to NaN
+                        str_spike_depths = spike_depths;
+                        str_spike_depths(spike_depths < str_depth(1) | spike_depths > str_depth(2)) = NaN;
+                        
+                        % Group the striatal spike depths into the um-standardized bins
+                        aligned_str_depth_group = discretize(str_spike_depths,str_depth_edges);
+                        
+                    end
+                end
+            end
+        end
+        
+    case 'kernel'
+        %%% Align by cortex-striatum kernel template
+        ephys_kernel_align_path = '\\zserver.cortexlab.net\Lab\Share\ajpeters\for_Julie\ephys_processing\';
+        ephys_kernel_align_fn = ['ephys_kernel_align_' num2str(n_aligned_depths) '_depths.mat'];        
+        if exist([ephys_kernel_align_path filesep ephys_kernel_align_fn],'file')
+            load([ephys_kernel_align_path filesep ephys_kernel_align_fn]);
+            % If alignment exists for this dataset, align
+            curr_animal_idx = strcmp(animal,{ephys_kernel_align.animal});
+            if any(curr_animal_idx)
+                curr_day_idx = strcmp(day,ephys_kernel_align(curr_animal_idx).days);
+                if any(curr_day_idx)
+                    if verbose; disp('Aligning striatum by kernel alignment...'); end
+                    % (use previously saved depth groups)
+                    aligned_str_depth_group = ephys_kernel_align(curr_animal_idx).aligned_str_depth_group{curr_day_idx};
+                    n_aligned_depths = ephys_kernel_align(curr_animal_idx).n_aligned_depths(curr_day_idx);
+                end
+            end
+        else
+            error(['No ephys alignment file: ' ephys_kernel_align_fn]);
+        end
+        % Check that there's an aligned group for every spike
+        if exist('aligned_str_depth_group','var') && length(spike_times) ~= length(aligned_str_depth_group)
+            error('Not 1:1 raw and aligned spike depths')
+        end
+        
+end
+
+% If alignment isn't available, return empty
+if ~exist('aligned_str_depth_group','var')
+    aligned_str_depth_group = [];
+end
+
+%% Plot the aligned groups and MUA correlation
+
+if verbose
+    % Plot MUA correlation    
+    figure;
+    imagesc(depth_corr_bin_centers,depth_corr_bin_centers,mua_corr);
+    axis tight equal;
+    colormap(hot)
+    line([str_depth(1),str_depth(1)],ylim,'color','b','linewidth',2);
+    line([str_depth(2),str_depth(2)],ylim,'color','b','linewidth',2);
+    line(xlim,[str_depth(1),str_depth(1)],'color','b','linewidth',2);
+    line(xlim,[str_depth(2),str_depth(2)],'color','b','linewidth',2);
+    xlabel('Probe depth (\mum)');
+    ylabel('Probe depth (\mum)');
+    title('MUA correlation: striatum location');
+    drawnow;
+end
+
+if verbose && exist('aligned_str_depth_group','var') && ...
+        ~isempty(aligned_str_depth_group)
+    % Plot group depth
+    [~,idx,~] = unique(spike_templates);
+    template_aligned_depth = aligned_str_depth_group(idx)+1;
+    template_aligned_depth(isnan(template_aligned_depth)) = 1;
+    
+    col = [0,0,1;copper(n_aligned_depths)];
+    
+    figure;plotSpread(template_depths,'distributionIdx', ...
+        template_aligned_depth,'distributionColors',col(unique(template_aligned_depth),:));
+    set(gca,'YDir','reverse');
+    line(xlim,[str_depth(1),str_depth(1)]);
+    line(xlim,[str_depth(2),str_depth(2)]);
+    drawnow;
+end
+
