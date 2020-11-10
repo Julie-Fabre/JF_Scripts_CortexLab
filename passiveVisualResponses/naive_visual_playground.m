d4a6e118a3ee9fbbee6be8afc191ef2ccdc89283
Julie-Fabre <julie.m.fabre@gmail.com>
2020-11-10 13:16:03 +0000

    queryAllenAtlas

--- passiveVisualResponses/naive_visual_playground.m
+++ passiveVisualResponses/naive_visual_playground.m
@@ -48,3 +48,18 @@ bb = boundary(c, v);
 plot(c(bb), v(bb));
 
 %% visual projections
+%get all V1 experiments
+expIDs = findAllenExperiments('injection', 'VISp', 'line', '0', 'primary', true);
+proj = getProjectionDataFromExperiment(expIDs(1));
+%plot proj to caudoputamen, projection_intensity, projection volume,
+%structure_id 
+
+[~, brain_outline] = plotBrainGrid([],[]);
+hold on;
+axis vis3d equal off manual
+view([-30,25]);
+caxis([0 300]);
+[ap_max,dv_max,ml_max] = size(tv);
+xlim([-10,ap_max+10])
+ylim([-10,ml_max+10])
+zlim([-10,dv_max+10])
