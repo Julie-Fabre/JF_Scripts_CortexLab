255243906fd657ae55dbdb8dec59899ac3509345
Julie-Fabre <julie.m.fabre@gmail.com>
2020-11-10 11:48:00 +0000

    strOutline

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
 Dependancies: 
 
--https://uk.mathworks.com/matlabcentral/fileexchange/181-keep#:~:text=KEEP%20is%20complementary%20to%20the%20%22clear%22%20command%3A%20instead%20of,workspace%20except%20x%20and%20y.
-
 -https://github.com/kwikteam/npy-matlab (to load data in)
 
 -https://github.com/petersaj/AP_scripts_cortexlab (to load data in and most other things)
--- passiveVisualResponses/naive_visual_playground.m
+++ passiveVisualResponses/naive_visual_playground.m
@@ -10,8 +10,9 @@ plot_structure_color = hex2dec(reshape(st.color_hex_triplet{curr_plot_structure}
 
 structure_3d = isosurface(permute(av(1:slice_spacing:end, ...
     1:slice_spacing:end, 1:slice_spacing:end) == curr_plot_structure, [3, 1, 2]), 0);
+structure_alpha = 0.2;
 
-figure(); 
+% 3-D plot 
 [~, brain_outline] = plotBrainGrid([],[]);
 hold on;
 axis vis3d equal off manual
@@ -21,17 +22,29 @@ caxis([0 300]);
 xlim([-10,ap_max+10])
 ylim([-10,ml_max+10])
 zlim([-10,dv_max+10])
-structure_alpha = 0.2;
 structure_patch = patch('Vertices', structure_3d.vertices*slice_spacing, ...
     'Faces', structure_3d.faces, ...
-    'FaceColor', plot_structure_color, 'EdgeColor', 'none', 'FaceAlpha', structure_alpha);
+    'FaceColor', 'k', 'EdgeColor', 'none', 'FaceAlpha', structure_alpha);
+scatter3(bregma(3), bregma(1), bregma(2));
 
-% 3-D
+% 2-D
+ii=permute(av(1:slice_spacing:end, ...
+    1:slice_spacing:end, 1:slice_spacing:end/2) == curr_plot_structure, [3, 1, 2]); % / 2 to only get one hemispehere
+[r,c,v] = ind2sub(size(ii),find(ii));
 
 % x-y
+figure();
+bb = boundary(r, c);
+plot(r(bb), c(bb));
 
 % x-z
+figure();
+bb = boundary(r, v);
+plot(r(bb), v(bb));
 
 % y-z
+figure();
+bb = boundary(c, v);
+plot(c(bb), v(bb));
 
 %% visual projections
