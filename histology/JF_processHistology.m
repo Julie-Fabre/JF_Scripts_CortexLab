a9bb51fb02bc3568e299bab50261d977a9703d84
Julie-Fabre <julie.m.fabre@gmail.com>
2020-11-21 19:00:52 +0000

    binDataForPlotting

--- histology/JF_processHistology.m
+++ histology/JF_processHistology.m
@@ -1,4 +1,4 @@
-animal = 'AP082';
+animal = 'JF019';
 
 %% get histology slices and copy locally
 locationHisto = ['//znas.cortexlab.net/Subjects/', animal, '/Histology/']; % copy files over to local disk
--- passiveVisualResponses/JF_process_naive_visual.m
+++ passiveVisualResponses/JF_process_naive_visual.m
@@ -1,7 +1,7 @@
 
 %% load naive visual recordings
 corona = 0;
-mice = {'AP080', 'AP082', 'JF017', 'JF019'}; % JF020 doesn't have histology yet. JF021 and JF022 to come
+mice = {'AP080', 'AP082', 'JF017', 'JF019'}; % JF020 doesn't have histology yet. JF021 and JF022 to come, and many more
 locations = {'CP', 'SNr', 'GPe', 'GPi', 'STN'};
 protocols = {'JF_natural_images', 'JF_locations', 'JF_GratingsPassive'};
 ephysData = struct;
@@ -68,7 +68,7 @@ for iMouse = 1:size(mice, 2)
                                 ephysData(thisCount).protocol = expDef;
                                 ephysData(thisCount).animal = animal;
                                 ephysData(thisCount).date = day;
-                                ephysData(thisCount).site = site;
+                                ephysData(thisCount).site = probe;
                                 ephysData(thisCount).experiment = experiment;
                                 ephysData(thisCount).location = locations(iLocation);
                                 thisCount = thisCount + 1;
@@ -80,8 +80,35 @@ for iMouse = 1:size(mice, 2)
         end
     end
 end
-%     indexes = probe_ccf .* 10;
-%     av(indexes)
-%     av(round(probe_ccf(:, 1)), probe_ccf(:, 2), probe_ccf(:, 3))
 
-%% plot responses
+
+%% plot responses - Striatum
+
+%find == 'CP'
+striatumData = find(arrayfun(@(x) contains(ephysData(x).location, 'CP'), 1:numel(ephysData)));
+
+%find striatum limits - allen atlas
+allen_atlas_path = 'C:\Users\Julie\Dropbox\Atlas\allenCCF';
+tv = readNPY([allen_atlas_path, filesep, 'template_volume_10um.npy']); % grey-scale "background signal intensity"
+av = readNPY([allen_atlas_path, filesep, 'annotation_volume_10um_by_index.npy']); % the number at each pixel labels the area, see note below
+st = loadStructureTree([allen_atlas_path, filesep, 'structure_tree_safe_2017.csv']); % a table of what all the labels mean
+curr_plot_structure = find(contains(st.name, 'audoputamen'));
+slice_spacing = 10;
+plot_structure_color = hex2dec(reshape(st.color_hex_triplet{curr_plot_structure}, 2, [])') ./ 255;
+
+%get limits and bins
+binSize = 100;
+structure_3d_lims = permute(av(1:slice_spacing:end, ...
+    1:slice_spacing:end, 1:slice_spacing:end) == curr_plot_structure, [3, 1, 2]);
+[r, c, v] = ind2sub(size(structure_3d_lims), find(structure_3d_lims));
+x_lim_um = [min(r), max(r)] .* 100;
+y_lim_um = [min(c), max(c)] .* 100;
+z_lim_um = [min(v), max(v)] .* 100;
+dataBinsX = x_lim_um(1):binSize:x_lim_um(2);
+dataBinsY = y_lim_um(1):binSize:y_lim_um(2);
+dataBinsZ = z_lim_um(1):binSize:z_lim_um(2);
+
+%find which bins striatal templates belong to 
+uniqueRecordings = arrayfun(@(x) [ephysData(x).animal, ephysData(x).date, num2str(ephysData(x).site)], 1:numel(ephysData),'UniformOutput', false);%unique date + site
+unique(uniqueRecordings, 'rows')
+%allTemplates = 
