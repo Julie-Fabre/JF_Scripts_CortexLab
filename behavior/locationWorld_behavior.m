25aaabd5696cf2b237a1dd69969611d8158eca1b
Julie-Fabre <julie.m.fabre@gmail.com>
2021-04-08 14:02:07 +0100

    frac no go

--- behavior/locationWorld_behavior.m
+++ behavior/locationWorld_behavior.m
@@ -9,7 +9,10 @@ for iAnimal = 1:size(animalsAll, 2)
     protocol = 'location'; %protocol name contains this name
     protocol2 = 'goNoGo';
     flexible_name = true; %protocol name can be slightly different
-    experiments = AP_find_experimentsJF(animal, protocol, protocol2); %find the experiments with the choiceworld protocol
+    experiments1 = AP_find_experimentsJF(animal, protocol, flexible_name); %find the experiments with the choiceworld protocol
+    experiments2 = AP_find_experimentsJF(animal, protocol2, flexible_name); %find the experiments with the choiceworld protocol
+    experiments(1:size(experiments1,1)) = experiments1;
+    experiments(size(experiments1,1)+1:size(experiments1,1)+size(experiments2,1)) = experiments2;
     %experiments(end)=[];
     bhv = struct; %initialize structure
     keep_day = [];
@@ -38,64 +41,95 @@ for iAnimal = 1:size(animalsAll, 2)
             load(block_filename)
             %correct non repeat trials
             response_trials = 1:length(block.events.responseValues);
-            repeatOnMisses = block.events.repeatTrialValues(response_trials) > 0;
+            if isfield(block.events, 'repeatTrialValues')
+                repeatOnMisses = block.events.repeatTrialValues(response_trials) > 0;
+            else
+                repeatOnMisses = block.events.repeatNumValues(response_trials) > 0;
+            end
 
-            correctTrialsLeft = block.events.trialSideValues(response_trials) == -1 & block.events.hitValues(response_trials) == 1;
-            correctTrialsRight = block.events.trialSideValues(response_trials) == 1 & block.events.hitValues(response_trials) == 1;
-            sum(correctTrialsLeft);
+            %correctTrialsLeft = block.events.trialSideValues(response_trials) == -1 & block.events.hitValues(response_trials) == 1;
+            if isfield(block.events, 'trialSideValues')
+                correctTrialsRight = block.events.trialSideValues(response_trials) == 1 & block.events.hitValues(response_trials) == 1;
+            else
+                correctTrialsRight = block.events.feedbackValues(response_trials) == 1;%go1 and go2
+            end
             sum(correctTrialsRight);
             n_trials = length(block.paramsValues);
             total_water = sum(block.outputs.rewardValues);
 
 
-            response_trials = 1:length(block.events.responseValues);
+            response_trials = 1:length(block.events.endTrialValues);
             clearvars correctTrials
             iImg = 1;
-            leftGood = block.events.sessionPerformanceValues(3, find(block.events.sessionPerformanceValues(1, :) == -1));
-            rightGood = block.events.sessionPerformanceValues(3, find(block.events.sessionPerformanceValues(1, :) == 1));
-            leftT = block.events.sessionPerformanceValues(2, find(block.events.sessionPerformanceValues(1, :) == -1));
-            rightT = block.events.sessionPerformanceValues(2, find(block.events.sessionPerformanceValues(1, :) == 1));
+            if isfield(block.events, 'trialSideValues')
+            %leftGood = block.events.sessionPerformanceValues(3, find(block.events.sessionPerformanceValues(1, :) == -1));
+                rightGood = block.events.sessionPerformanceValues(3, find(block.events.sessionPerformanceValues(1, :) == 1));
+                %leftT = block.events.sessionPerformanceValues(2, find(block.events.sessionPerformanceValues(1, :) == -1));
+                rightT = block.events.sessionPerformanceValues(2, find(block.events.sessionPerformanceValues(1, :) == 1));
+            else
+                block.events.trialSideValues(response_trials) = 1;
+                hitTimesPossib = [block.events.feedbackTimes, block.events.noGoQuiescenceTimes];
+                hitValuesUnsorted = [ones(size(block.events.feedbackTimes,2),1)', zeros(size(block.events.noGoQuiescenceTimes,2),1)'];
+                [sortedV, sortedIdx] = sort(hitTimesPossib);
+                block.events.hitValues(response_trials) = hitValuesUnsorted(sortedIdx);
+            end
+            if contains(block.expDef, 'goNoGo')
+                go1Trials = block.events.trialTypeValues(response_trials) == 1; 
+                go2Trials = block.events.trialTypeValues(response_trials) == 2; 
+                noGoTrials = block.events.trialTypeValues(response_trials) == 3; 
+            else
+                go1Trials = ones(size(block.events.trialSideValues(response_trials),2),1)'; 
+                go2Trials = zeros(size(block.events.trialSideValues(response_trials),2),1)'; 
+                noGoTrials = zeros(size(block.events.trialSideValues(response_trials),2),1)'; 
+            end 
+            
+            correctTrials((iImg - 1)*3+1) =sum(block.events.trialSideValues(response_trials) == 1 & ...
+                block.events.hitValues(response_trials) == 1 & go1Trials...
+                );% go 1
+            correctTrials((iImg - 1)*3+2) = sum(block.events.trialSideValues(response_trials) == 1 & ...
+                block.events.hitValues(response_trials) == 1 & go2Trials...
+                ); % go 2           
+            correctTrials((iImg - 1)*3+3) =sum(block.events.trialSideValues(response_trials) == 1 & ...
+                block.events.hitValues(response_trials) == 1 & noGoTrials...
+                );% no go 
 
-            correctTrials((iImg - 1)*2+1) = sum(block.events.trialSideValues(response_trials) == -1 & ...
-                block.events.hitValues(response_trials) == 1);
-
-            correctTrials((iImg - 1)*2+2) = sum(block.events.trialSideValues(response_trials) == 1 & ...
-                block.events.hitValues(response_trials) == 1 ...
+            nTrials((iImg - 1)*3+1) = sum(~[repeatOnMisses(response_trials(2:end)), 0] & ...
+                block.events.trialSideValues(response_trials) == -1 & go1Trials...
+                );
+            nTrials((iImg - 1)*3+2) = sum(~[repeatOnMisses(response_trials(2:end)), 0] & ...
+                block.events.trialSideValues(response_trials) == 1 & go2Trials...
+                );          
+            nTrials((iImg - 1)*3+3) = sum(~[repeatOnMisses(response_trials(2:end)), 0] & ...
+                block.events.trialSideValues(response_trials) == 1 & noGoTrials...
                 );
 
-
-            %                     bhv.n_trials_condition(curr_day, :) = performance(2, :);
-            %                 bhv.go_left_trials(curr_day, :) = performance(end, :);
-            %
-            %                     correctTrials((iImg-1)*2+1) = sum(~repeatOnMisses(response_trials) & block.events.trialSideValues(response_trials) == -1 & block.events.hitValues(response_trials) == 1 ...
-            %                         & block.events.stimNValues(response_trials) == imgs(iImg));
-            %                      nTrials((iImg-1)*2+1) = sum(diff(rightT([response_trials,response_trials(end)+1])) ==1 &...
-            %                          block.events.stimNValues(response_trials) == imgs(iImg));
-            % %                       correctTrials((iImg-1)*2+2) = sum(~repeatOnMisses(response_trials) & block.events.trialSideValues(response_trials) == 1 & block.events.hitValues(response_trials) == 1 ...
-            % %                         & block.events.stimNValues(response_trials) == imgs(iImg));
-            %
-            %                     nTrials((iImg-1)*2+2) = sum(diff(leftT([response_trials,response_trials(end)+1])) ==1 &...
-            %                          block.events.stimNValues(response_trials) == imgs(iImg));
-
-            nTrials((iImg - 1)*2+1) = sum(~[repeatOnMisses(response_trials(2:end)), 0] & ...
-                block.events.trialSideValues(response_trials) == -1 ...
-                );
-
-            nTrials((iImg - 1)*2+2) = sum(~[repeatOnMisses(response_trials(2:end)), 0] & ...
-                block.events.trialSideValues(response_trials) == 1 ...
-                );
-
-            conditi = [-1, 1];
+            conditi = [1,2,3];
 
 
-            goLeft((iImg - 1)*2+1) = sum(~[repeatOnMisses(response_trials(2:end)), 0] & ...
-                block.events.trialSideValues(response_trials) == -1 & ...
-                block.events.hitValues(response_trials) == 0 ...
-                );
-
-            goLeft((iImg - 1)*2+2) = sum(~[repeatOnMisses(response_trials(2:end)), 0] & ...
+            goLeft((iImg - 1)*3+1) = sum(~[repeatOnMisses(response_trials(2:end)), 0] & ...
                 block.events.trialSideValues(response_trials) == 1 & ...
-                block.events.hitValues(response_trials) == 1 ...
+                block.events.hitValues(response_trials) == 1 & go1Trials...
+                );
+            goLeft((iImg - 1)*3+2) = sum(~[repeatOnMisses(response_trials(2:end)), 0] & ...
+                block.events.trialSideValues(response_trials) == 1 & ...
+                block.events.hitValues(response_trials) == 1 & go2Trials...
+                );
+            goLeft((iImg - 1)*3+3) = sum(~[repeatOnMisses(response_trials(2:end)), 0] & ...
+                block.events.trialSideValues(response_trials) == 1 & ...
+                block.events.hitValues(response_trials) == 1 & noGoTrials...
+                );
+            
+            noGo((iImg - 1)*3+1) = sum(~[repeatOnMisses(response_trials(2:end)), 0] & ...
+                block.events.trialSideValues(response_trials) == 1 & ...
+                block.events.hitValues(response_trials) == 0 & go1Trials...
+                );
+            noGo((iImg - 1)*3+2) = sum(~[repeatOnMisses(response_trials(2:end)), 0] & ...
+                block.events.trialSideValues(response_trials) == 1 & ...
+                block.events.hitValues(response_trials) == 0 & go2Trials...
+                );
+            noGo((iImg - 1)*3+3) = sum(~[repeatOnMisses(response_trials(2:end)), 0] & ...
+                block.events.trialSideValues(response_trials) == 1 & ...
+                block.events.hitValues(response_trials) == 0 & noGoTrials...
                 );
             
             
@@ -134,19 +168,13 @@ for iAnimal = 1:size(animalsAll, 2)
             movingFrac = cp(end, :) / size(binArray,1);
 
            
-            %                     goLeft((iImg-1)*2+1) = sum(block.events.trialSideValues(response_trials) == -1 & ...
-            %                         diff(leftGood([response_trials,response_trials(end)+1])) == 0 &...
-            %                         block.events.stimNValues(response_trials) == imgs(iImg));
-            %                     goLeft((iImg-1)*2+2) = sum(block.events.trialSideValues(response_trials) == 1 &...
-            %                         diff(rightGood([response_trials,response_trials(end)+1])) > 0 &...
-            %                         block.events.stimNValues(response_trials) == imgs(iImg));
-
             bhv.correctTrials(curr_day, :) = correctTrials;
             bhv.nTrials(curr_day, :) = nTrials;
             bhv.n_trials(curr_day, :) = n_trials;
             bhv.total_water(curr_day, :) = total_water;
             bhv.conditions(curr_day, :) = conditi;
             bhv.goLeft(curr_day, :) = goLeft;
+            bhv.noGo(curr_day, :) = noGo; 
             keep_day = [keep_day, curr_day];
 
 
@@ -159,7 +187,7 @@ for iAnimal = 1:size(animalsAll, 2)
         {experiments(keep_day).day}, 'uni', false);
 
     figure('Name', animal);
-    subplot(211)
+    subplot(311)
     yyaxis left
 
     scatter(day_num, bhv.n_trials(keep_day, :));
@@ -181,7 +209,7 @@ for iAnimal = 1:size(animalsAll, 2)
     set(gca, 'XTickLabelRotation', 90);
     makepretty;
 
-    subplot(212)
+    subplot(312)
     con = bhv.conditions(keep_day(1), :);
     ccc = num2str(con(1, :));
     cc = textscan(ccc, '%s', 'Delimiter', ' ');
@@ -193,6 +221,33 @@ for iAnimal = 1:size(animalsAll, 2)
     colormap(brewermap([], '*RdBu'));
     c = colorbar;
     ylabel(c, 'Go left (frac)');
+    xlabel('< 0 = left, > 0 = right');
+    ylabel('Session');
+    set(gca, 'XTick', 1:length(con));
+    set(gca, 'XTickLabel', cc);
+    set(gca, 'YTick', 1:length(keep_day));
+    set(gca, 'YTickLabel', day_labels);
+
+    for iDay = 1:size(keep_day, 2)
+        txt = num2str(bhv.correctTrials(keep_day(iDay), :)');
+        text((1:length(con))-0.2, ones(1, length(con))*iDay, txt, 'BackgroundColor', 'w')
+    end
+    %axis square;
+    caxis([0, 1])
+    makepretty;
+    
+    subplot(313)
+    con = bhv.conditions(keep_day(1), :);
+    ccc = num2str(con(1, :));
+    cc = textscan(ccc, '%s', 'Delimiter', ' ');
+    cc = cc{1};
+    cc(strcmp('', cc)) = [];
+    im = imagesc(1:length(con), 1:size(bhv.correctTrials(keep_day, :), 1), bhv.noGo(keep_day, :)./bhv.nTrials(keep_day, :));
+    set(im, 'AlphaData', ~isnan(get(im, 'CData')));
+    set(gca, 'color', [0.5, 0.5, 0.5]);
+    colormap(brewermap([], '*RdBu'));
+    c = colorbar;
+    ylabel(c, 'No go (frac)');
     xlabel('< 0 = left, > 0 = right');
     ylabel('Session');
     set(gca, 'XTick', 1:length(con));
