4283210b0d2ae59cc2fcbc6b5d34bf9ccf4bbba2
Julie-Fabre <julie.m.fabre@gmail.com>
2023-05-27 12:45:20 +0100

    selectivity: no baseline subtracting

---
+++ clean-WIP/cl_plottingSettings.m
@@ -0,0 +1,7 @@
+%% cl_plottingSettings
+
+regions = {'CP', 'GPe', 'GPi', 'STN', 'SNr', 'SNc', 'VTA'};
+
+regionColors = {rgb('DeepSkyBlue'); rgb('SeaGreen'); rgb('DarkOrange'); rgb('Crimson'); rgb('Hotpink'); rgb('Black'); rgb('Brown')};
+
+regionLineStyle = {'-', '--', ':', '--', ':', '-.', '-.'};
--- clean-WIP/cl_selectivity.m
+++ clean-WIP/cl_selectivity.m
@@ -19,10 +19,24 @@
 % the true values of the population, they do not reduce the impact of
 % selection on those metrics.
 
+% from hikosaka https://www.jneurosci.org/content/39/9/1709:
+% Object selectivity of visual neurons in the caudal putamen and caudate tail. 
+% A, In the passive-viewing task, fractal objects (no value associated) were 
+% presented sequentially in the preferred location of the neuron, while the 
+% monkey was fixating at the center. The reward was not associated with 
+% particular objects. B, Object-selective responses of a PUTt neuron from 
+% monkey S to fractal objects during the passive-viewing task. C–E, Distributions 
+% of the object selectivity index among neurons in cdPUT (C), PUTt (D), and CDt 
+% (E). The neurons showing significant object selectivity are indicated by black 
+% (ANOVA, p < 0.05). F, Object selectivity in each region (mean and 95% 
+% confidence interval). The across-region difference is also shown (Scheffé 
+% test, *p < 0.05).
+
 % for each unit, get maximum response on half trials, and then calculate
 % selectivity index based on this
 % matrix of neurons mean response to each condition. use absolute value
 % (mean subtracted)
+
 
 % for each condition, measure
 keep passive_data regions
@@ -39,10 +53,26 @@ for iRegion = 1:size(regions,2)
             use_conditions = 1:size(passive_data.psth{iCondition},3);
         end
         for iUnit = 1:size(curr_units, 1)
+  %          iUnit = iUnit+1
             thisUnit = curr_units(iUnit);
+%             baseline_per_cond = squeeze(nanmean(passive_data.psth{iCondition}(thisUnit, 1, use_conditions, 1:50), 4));
+%             baseline_sub_average_per_cond = arrayfun(@(x) nanmean(abs(squeeze(passive_data.psth{iCondition}(thisUnit, 1, x, 55:70)) ...
+%                 -baseline_per_cond(x))+baseline_per_cond(x)), 1:size(baseline_per_cond, 1));
+% 
+%             max_half_trials = find(baseline_sub_average_per_cond == max(baseline_sub_average_per_cond));
+%             if length(max_half_trials) > 1
+%                 max_half_trials = max_half_trials(1);
+%             end
+% 
+%             baseline_per_cond_cv = squeeze(nanmean(passive_data.psth{iCondition}(thisUnit, 2, use_conditions, 1:50), 4));
+%             baseline_sub_average_per_cond_cv = arrayfun(@(x) nanmean(abs(squeeze(passive_data.psth{iCondition}(thisUnit, 2, x, 55:70)) ...
+%                 -baseline_per_cond_cv(x))+baseline_per_cond_cv(x)), 1:size(baseline_per_cond_cv, 1));
+%             selectivity_index{iRegion}(thisUnit, iCondition) = abs((baseline_sub_average_per_cond_cv(max_half_trials) -...
+%                 nanmean(baseline_sub_average_per_cond_cv))./ ...
+%                 nanmax(baseline_sub_average_per_cond_cv)); % (c.v. max  - mean ) / max
+
             baseline_per_cond = squeeze(nanmean(passive_data.psth{iCondition}(thisUnit, 1, use_conditions, 1:50), 4));
-            baseline_sub_average_per_cond = arrayfun(@(x) nanmean(abs(squeeze(passive_data.psth{iCondition}(thisUnit, 1, x, 55:70)) ...
-                -baseline_per_cond(x))), 1:size(baseline_per_cond, 1));
+            baseline_sub_average_per_cond = nanmean(abs(squeeze(passive_data.psth{iCondition}(thisUnit, 1, :, 55:70))));
 
             max_half_trials = find(baseline_sub_average_per_cond == max(baseline_sub_average_per_cond));
             if length(max_half_trials) > 1
@@ -50,16 +80,23 @@ for iRegion = 1:size(regions,2)
             end
 
             baseline_per_cond_cv = squeeze(nanmean(passive_data.psth{iCondition}(thisUnit, 2, use_conditions, 1:50), 4));
-            baseline_sub_average_per_cond_cv = arrayfun(@(x) nanmean(abs(squeeze(passive_data.psth{iCondition}(thisUnit, 2, x, 55:70)) ...
-                -baseline_per_cond_cv(x))+baseline_per_cond_cv(x)), 1:size(baseline_per_cond_cv, 1));
-            selectivity_index{iRegion}(thisUnit, iCondition) = abs((baseline_sub_average_per_cond_cv(max_half_trials) - nanmean(baseline_sub_average_per_cond_cv))./ ...
+            baseline_sub_average_per_cond_cv = nanmean(abs(squeeze(passive_data.psth{iCondition}(thisUnit, 2, :, 55:70)))) ;
+            selectivity_index{iRegion}(thisUnit, iCondition) = abs((baseline_sub_average_per_cond_cv(max_half_trials) -...
+                nanmean(baseline_sub_average_per_cond_cv))./ ...
                 nanmax(baseline_sub_average_per_cond_cv)); % (c.v. max  - mean ) / max
+
+% clf;
+% hold on;
+% for ii =1:5
+% plot(squeeze(passive_data.psth{iCondition}(thisUnit, 2, ii, :)))
+% end
+% plot(squeeze(nanmean(passive_data.psth{iCondition}(thisUnit, 2, :, :))), 'LineWidth',2)
 
         end
     end
 end
 
-theseColors = {rgb('DeepSkyBlue'); rgb('SeaGreen'); rgb('DarkOrange'); rgb('Crimson'); rgb('Hotpink'); rgb('Black'); rgb('Brown')};
+cl_plottingSettings;
 
 figure();
 for iRegion = 1:size(regions,2)
@@ -68,13 +105,17 @@ for iRegion = 1:size(regions,2)
         hold on;
         [N,edges,bin] = histcounts(selectivity_index{iRegion}(:,iCondition), 0:0.05:1);
         hist_bin = edges(1:end-1) + diff(edges) ./ 2;
-        stairs(hist_bin, N./length(bin), 'Color', theseColors{iRegion}, 'LineWidth', 2)
+        stairs(hist_bin, N./length(bin), 'Color', regionColors{iRegion}, 'LineWidth', 2, 'LineStyle', regionLineStyle{iRegion})
         
         if iRegion == 1 && iCondition == 1
             ylabel('fraction of cells')
             xlabel('c.v. selectivity index')
         end
         axis square;
+        % add median 
+        ylim([0 1.2])
+        line([nanmedian(selectivity_index{iRegion}(:,iCondition)), ...
+           nanmedian(selectivity_index{iRegion}(:,iCondition))],[1, 1.2], 'Color', regionColors{iRegion}, 'LineWidth', 2, 'LineStyle', regionLineStyle{iRegion});
         makepretty; 
     end
 end
