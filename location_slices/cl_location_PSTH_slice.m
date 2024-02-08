23b9bf36c3fc129833e4e56c79b0dbdbc4cd6046
Julie-Fabre <julie.m.fabre@gmail.com>
2024-02-08 14:46:05 +0000

    location slices - visualize AP chunks in ML x AP projection

--- location_slices/cl_location_PSTH_slice.m
+++ location_slices/cl_location_PSTH_slice.m
@@ -4,7 +4,7 @@ passive_data = load('/home/julie/Dropbox/MATLAB/naive_data1.mat');
 regions = {'CP', 'GPe', 'SNr'};
 
 keep passive_data regions
-close all;
+figure(1);clf;
 
 cl_myPaths;
 regionResolution = [1, 1, 1, 1, 1, 1, 1];
@@ -30,28 +30,41 @@ if ~exist('st', 'var')
 end
 
 structure_alpha = 0.2;
-nChunks = 3
-region_ap_boundaries = [];
-chunks_region = [152.0000, 203.6667, 255.3333, 307.0000]; %for Striatum for now
 
-%% X-Y, X-Z, Y-Z plots for each region.
+%% get each region's limits and define chunks
+nChunks = 4;
+clearvars chunks_region
+% for iRegion = 1:size(regions, 2)
+%     clearvars regionLocation
+%     curr_plot_structure = st.id(strcmp(st.acronym, regions{iRegion}));
+% 
+%     region_area = permute(av(1:1:end, ...
+%         1:1:end, 1:1:end/2) == curr_plot_structure, [3, 1, 2]); % / 2 to only get one hemispehere
+%     % AP, DV, ML -> ML, AP, DV
+% 
+%     [regionLocation(1, :), regionLocation(2, :), regionLocation(3, :)] ...
+%         = ind2sub(size(region_area), find(region_area)); %ML, AP, DV
+% 
+%     region_ap_boundaries(iRegion, :) = [min(regionLocation(:, 2)), max(regionLocation(:, 2))];
+% 
+%     chunks_region(iRegion,:) = region_ap_boundaries(iRegion, 1):(region_ap_boundaries(iRegion, 2)-region_ap_boundaries(iRegion, 1))/nChunks:region_ap_boundaries(iRegion, 2);
+% end
+region_ap_boundaries = [152, 307;...
+    219,287;...
+    311,363];
 
+for iRegion = 1:size(regions, 2)
+    chunks_region(iRegion,:) =  region_ap_boundaries(iRegion, 1):(region_ap_boundaries(iRegion, 2)-region_ap_boundaries(iRegion, 1))/nChunks:region_ap_boundaries(iRegion, 2);
+end
+
+%% Visualize AP chunks in ML x AP projection
 for iRegion = 1:size(regions, 2)
 
     curr_plot_structure = st.id(strcmp(st.acronym, regions{iRegion}));
-    %     structure_3d = isosurface(permute(av(1:regionSpacing(iRegion):end, ...
-    %         1:regionSpacing(iRegion):end, 1:regionSpacing(iRegion):end) == curr_plot_structure, [3, 1, 2]), 0);
-
-
-    % seperate DV into 3 chunks
-    ap_idx = 2;
-    %chunks = min(regionLocation(ap_idx,:)):...
-    %     (max(regionLocation(ap_idx,:)) - min(regionLocation(ap_idx,:)))/nChunks:...
-    %    max(regionLocation(ap_idx,:));
 
     figure(1);
 
-    projection_views = [1, 2]; %[1,2; 1,3; 2,3];%ML/AP, ML/DV, AP/DV
+    projection_views = repmat([1,2],nChunks, 1);%[1,2; 1,3; 2,3];%ML/AP, ML/DV, AP/DV
 
 
     % initialize variables
@@ -71,7 +84,65 @@ for iRegion = 1:size(regions, 2)
         thisChunk_AP = regionLocation(projection_views(1, 1), :);
         thisChunk_DV = regionLocation(projection_views(1, 2), :);
 
-        subplot(3, size(regions, 2), iRegion+(size(regions, 2) * (iChunk - 1)))
+        subplot(nChunks, size(regions, 2), iRegion+(size(regions, 2) * (4 - iChunk)))
+        boundary_projection{iChunk} = boundary(thisChunk_AP', ...
+            thisChunk_DV', 0);
+
+        plot(regionLocation(projection_views(1, 1), boundary_projection{iChunk})./10, ...
+            regionLocation(projection_views(1, 2), boundary_projection{iChunk})./10, ...
+            'Color', theseColors{iRegion});
+        axis equal
+        axis square
+        axis image
+        makepretty;
+
+        projection_view_lims(iChunk, 1, :) = xlim .* 10;
+        projection_view_lims(iChunk, 2, :) = ylim .* 10;
+        projection_view_bins{iChunk} = {projection_view_lims(iChunk, 1, 1): ...
+            (projection_view_lims(iChunk, 1, 2) - projection_view_lims(iChunk, 1, 1)) / 15: ...
+            projection_view_lims(iChunk, 1, 2), ...
+            projection_view_lims(iChunk, 2, 1): ...
+            (projection_view_lims(iChunk, 2, 2) - projection_view_lims(iChunk, 2, 1)) / 15: ...
+            projection_view_lims(iChunk, 2, 2)};
+
+    end
+
+    theseLocations = passive_data.unit_coords;
+    theseLocationsBregmaAbs = [(theseLocations(:, 3)), ...
+        theseLocations(:, 1), ...
+        theseLocations(:, 2)]; %AP, DV, ML -> ML, AP, DV
+end
+
+
+%% AP chunks in ML x DV projection 
+
+for iRegion = 1:size(regions, 2)
+
+    curr_plot_structure = st.id(strcmp(st.acronym, regions{iRegion}));
+
+    figure(1);
+
+    projection_views = repmat([1,2],nChunks, 1);%[1,2; 1,3; 2,3];%ML/AP, ML/DV, AP/DV
+
+
+    % initialize variables
+    boundary_projection = cell(3, 1);
+    projection_view_bins = cell(3, 1);
+    projection_view_lims = nan(3, 2, 2);
+
+    for iChunk = 1:nChunks
+        clearvars regionLocation
+        % get structure boundaries and plot outline
+        region_area = permute(av(round(chunks_region(iRegion, iChunk)):1:round(chunks_region(iRegion, iChunk+1)), ...
+            1:1:end, 1:1:end/2) == curr_plot_structure, [3, 1, 2]); % / 2 to only get one hemispehere
+        % AP, DV, ML -> ML, AP, DV
+
+        [regionLocation(1, :), regionLocation(2, :), regionLocation(3, :)] ...
+            = ind2sub(size(region_area), find(region_area)); %ML, AP, DV
+        thisChunk_AP = regionLocation(projection_views(1, 1), :);
+        thisChunk_DV = regionLocation(projection_views(1, 2), :);
+
+        subplot(nChunks, size(regions, 2), iRegion+(size(regions, 2) * (4 - iChunk)))
         boundary_projection{iChunk} = boundary(thisChunk_AP', ...
             thisChunk_DV', 0);
 
@@ -101,7 +172,7 @@ for iRegion = 1:size(regions, 2)
 
     %% plot average increase for each bin
 
-    for iChunk = 1:3
+    for iChunk = 1:nChunks
         [N, Xedges, Yedges, binX, binY] = histcounts2(theseLocationsBregmaAbs(:, projection_views(iChunk, 1)), ...
             theseLocationsBregmaAbs(:, projection_views(iChunk, 2)), projection_view_bins{iChunk}{1}, ...
             projection_view_bins{iChunk}{2}); %par rapport a bregma!
@@ -132,6 +203,14 @@ for iRegion = 1:size(regions, 2)
         binnedArrayPixelSmooth = binnedArrayPixel;
 
         % remove any data points outside of the ROI
+         clearvars regionLocation
+        % get structure boundaries and plot outline
+        region_area = permute(av(round(chunks_region(iRegion, iChunk)):-1:round(chunks_region(iRegion, iChunk+1)), ...
+            1:1:end, 1:1:end/2) == curr_plot_structure, [3, 1, 2]); % / 2 to only get one hemispehere
+        % AP, DV, ML -> ML, AP, DV
+
+        [regionLocation(1, :), regionLocation(2, :), regionLocation(3, :)] ...
+            = ind2sub(size(region_area), find(region_area)); %ML, AP, DV
         isIN = nan(size(binnedArrayPixelSmooth, 1), size(binnedArrayPixelSmooth, 2));
         for iPixelX = 1:size(binnedArrayPixelSmooth, 1)
             for iPixelY = 1:size(binnedArrayPixelSmooth, 2)
@@ -143,7 +222,7 @@ for iRegion = 1:size(regions, 2)
         end
 
         figure(1)
-        subplot(3, size(regions, 2), iRegion+(size(regions, 2) * (iChunk - 1)))
+        subplot(nChunks, size(regions, 2), iRegion+(size(regions, 2) * (iChunk - 1)))
 
         binnedArrayPixelSmooth(isIN == 0) = mean(thisCmap_limits);
         ax = gca;
@@ -191,5 +270,5 @@ for iRegion = 1:size(regions, 2)
         xlim([projection_view_bins{iChunk}{1}(1), projection_view_bins{iChunk}{1}(end)])
         ylim([projection_view_bins{iChunk}{2}(1), projection_view_bins{iChunk}{2}(end)])
     end
-    keep passive_data regions thisCmap_limits st av regionResolution structure_alpha theseColors dFR_psth iRegion bregma
+    keep passive_data regions thisCmap_limits st av regionResolution structure_alpha theseColors dFR_psth iRegion bregma nChunks chunks_region
 end
